c0e9f5b84a17bfbb8680027cb4061af4504cc4c2
Momodou Berlemann <mberlemann@tso.de>
2022-06-26 16:34:15 +0200

    Kommentare

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "SudokuLibrary/library.h"
 #include <stdio.h>
 
+// Falls library.h nicht gefunden wird, müssen library.h und library.c in den selben Ordner mit der main.c gelegt werden 
+// und das include in der main.c muss auf #include library.h geändert werden. 
+
 int main() {
     initialize_structs();
     
